32985a563d9a4e603a37ba1e98c80ee79c617a56
terrydeng666 <terry10428@gmail.com>
2021-08-13 19:51:31 +0800

    implement 1/2

--- data_structure/LinkedList/linkedlist.cpp
+++ data_structure/LinkedList/linkedlist.cpp
@@ -24,4 +24,71 @@
      Node<T>* head;
      Node<T>* tail;
      int size;
+     bool isEmpty;
  };
+
+ template <typename T>
+ LinkedList<T>::LinkedList() 
+ {
+     head=nullptr;
+     tail=nullptr;
+     size=0;
+     isEmpty=true;
+ }
+ template <typename T>
+ LinkedList<T>::LinkedList(const LinkedList<T> &l) 
+ {
+     this->head=(T*)malloc(sizeof(T));
+     memcpy(this->head,l.head,sizeof(T)*l.size);
+     this->tail = this->head + size;
+     this->size=l.size;
+     this->isEmpty=false;
+ }
+template <typename T>
+ LinkedList<T>::~LinkedList() 
+ {
+     free(this->head);
+     free(this->tail);
+ }
+ template <typename T>
+ void LinkedList<T>::Clear() 
+ {
+     head=(T*)realloc(head,sizeof(T));
+     tail=head;
+     size=0;
+     isEmpty=true;
+ }
+ template <typename T>
+ void LinkedList<T>::Push_front(const T &item) 
+ {
+     Node<T>* node;
+     node->data=item;
+     if(isEmpty) {
+         this->head=node;
+         this->tail=node;
+         isEmpty=false;
+     }
+     else {
+        node->next=this->head;
+        this->head=node;
+     }
+     return;
+ }
+ template <typename T>
+ void LinkedList<T>::Push_back(const T &item) 
+ {
+     Node<T>* node;
+     node->data=item;
+     if(isEmpty) {
+         this->head=node;
+         this->tail=node;
+         isEmpty=false;
+     }
+     else {
+         tail->next=node;
+         tail=node;
+     }
+     tail->next=nullptr;
+     return;
+ }
+
