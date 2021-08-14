02ca70a680803712b5f0d8036e704c02bac4de01
terrydeng666 <terry10428@gmail.com>
2021-08-14 16:53:23 +0800

    done

--- data_structure/LinkedList/linkedlist.cpp
+++ data_structure/LinkedList/linkedlist.cpp
@@ -1,7 +1,10 @@
 #include <bits/stdc++.h>
+template<typename T>
+class LinkedList;
+
  template<typename T>
  class Node{
-    friend class LinkedLiist;
+    friend class LinkedList<T>;
     private:
     T data;
     Node<T>* next;
@@ -11,7 +14,8 @@
      public:
      LinkedList();
      LinkedList(const LinkedList<T> &l);
-     ~LinkedList();
+     //~LinkedList();
+     // LinkedList& operator = (const LinkedList<T> &d);
      void Clear();
      void Push_front(const T &item);
      void Push_back(const T &item);
@@ -38,22 +42,22 @@
  template <typename T>
  LinkedList<T>::LinkedList(const LinkedList<T> &l) 
  {
-     this->head=(T*)malloc(sizeof(T));
-     memcpy(this->head,l.head,sizeof(T)*l.size);
-     this->tail = this->head + size;
+     this->head=l.head;
+     this->tail=l.tail;
      this->size=l.size;
      this->isEmpty=false;
  }
-template <typename T>
+/*template <typename T>
  LinkedList<T>::~LinkedList() 
  {
-     free(this->head);
-     free(this->tail);
- }
+     delete head;
+     delete tail;
+ }*/
  template <typename T>
  void LinkedList<T>::Clear() 
  {
-     head=(T*)realloc(head,sizeof(T));
+     delete head;
+     head = new Node<T>;
      tail=head;
      size=0;
      isEmpty=true;
@@ -61,7 +65,7 @@ template <typename T>
  template <typename T>
  void LinkedList<T>::Push_front(const T &item) 
  {
-     Node<T>* node;
+     Node<T>* node=new Node<T>;
      node->data=item;
      if(isEmpty) {
          this->head=node;
@@ -72,12 +76,13 @@ template <typename T>
         node->next=this->head;
         this->head=node;
      }
+     size++;
      return;
  }
  template <typename T>
  void LinkedList<T>::Push_back(const T &item) 
  {
-     Node<T>* node;
+     Node<T>* node=new Node<T>;
      node->data=item;
      if(isEmpty) {
          this->head=node;
@@ -89,6 +94,93 @@ template <typename T>
          tail=node;
      }
      tail->next=nullptr;
+     size++;
+     return;
+ }
+ template <typename T>
+ void LinkedList<T>::insert(const int index ,const T &item) 
+ {
+     if(index>size-1) {
+         std::cout << "out of boundary,FAQ";
+         return;
+     }
+     Node<T>* temp=new Node<T>;
+     Node<T>* node=new Node<T>;
+     if(index==size-1) {
+         tail->next=node;
+         node->next=nullptr;
+         node->data=item;
+         size++;
+     }
+     node->data=item;
+     int i=0;
+     for(temp=head;i<index-1;i++,temp=temp->next);
+     node->next=temp->next;
+     temp->next=node;
+     size++;
+     return;
+ }
+ template <typename T>
+ void LinkedList<T>::delete_node(const int index)
+ {
+     if(index>size-1) {
+         std::cout << "out of boundary,FAQ";
+         return;
+     }
+     Node<T>* temp=new Node<T>;
+     Node<T>* node=new Node<T>;
+     int i=0;
+     for(temp=head;i<index-1;i++,temp=temp->next);
+     //i=index-1
+     node=temp->next;
+     temp->next=node->next;
+     delete node;
+     size--;
      return;
  }
 
+template <typename T>
+ void LinkedList<T>::print_list() 
+ {
+     Node<T>* node = new Node<T>;
+     int i=0;
+     for(node = head;i<size;i++,node = node->next) {
+         std::cout << node->data << std::endl;
+     }
+ }
+ template <typename T>
+ int LinkedList<T>::getsize() {
+     return size;
+ }
+/*template<typename T>
+ LinkedList<T> &LinkedList<T>::operator = (const LinkedList<T> &l) 
+ {
+     if(this == &l) return *this;
+     if(l.isEmpty) Clear();
+     head=nullptr;
+     tail=nullptr;
+     return *this;
+
+ }
+ */
+
+ int main(void) {
+     LinkedList<int> intL;
+     LinkedList<char> charL;
+     intL.Push_front(1);
+     charL.Push_front('a');
+     intL.print_list();
+     charL.print_list();
+     LinkedList<int> intL2(intL);
+     intL2.print_list();
+     intL.Push_front(0);
+     intL.print_list();
+     intL.Push_back(2);
+     intL.print_list();
+     std::cout << intL.getsize()<< std::endl;
+     intL.insert(1,22);
+     intL.print_list();
+     intL.delete_node(1);
+     intL.print_list();
+     return 0;
+ }
